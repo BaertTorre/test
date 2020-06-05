247f614358223ec65d7fe41ab6ef141273fa0b10
BaertTorre <torre.baert@student.howest.be>
2020-06-05 23:51:02 +0200

    read pwm

--- read_PWM.py
+++ read_PWM.py
@@ -1,4 +1,52 @@
 import time
 import pigpio
-import read_PWM
+from RPi import GPIO
 
+PWM_GPIO = 13
+
+class PWM_reader:
+    def __init__(self, pi, gpio):
+        self._high_tick = None
+        self._high = None
+    
+        self._cb = pi.callback(gpio, pigpio.EITHER_EDGE, self._cbf)     # soort van event detector
+ 
+    def _cbf(self, gpio, level, tick):
+        if level == 1:          
+            if self._high_tick is not None:
+                t = pigpio.tickDiff(self._high_tick, tick)
+            self._high_tick = tick
+    
+        elif level == 0:    
+            if self._high_tick is not None:
+                t = pigpio.tickDiff(self._high_tick, tick)
+                self._high = t
+ 
+    def pulse_width(self):
+        """
+        Returns the PWM pulse width in microseconds.
+        """
+        if self._high is not None:
+            return self._high
+        else:
+            return 0.0
+ 
+    def cancel(self):
+        """
+        Cancels the reader and releases resources.
+        """
+        self._cb.cancel()
+ 
+try:
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(PWM_GPIO, GPIO.IN)
+    pi = pigpio.pi()
+    channel1 = PWM_reader(pi, PWM_GPIO)
+    while True:
+        print(channel1.pulse_width())
+        time.sleep(0.1)
+except KeyboardInterrupt as e:
+    print(e)
+finally:
+    channel1.cancel()
+    pi.stop()
