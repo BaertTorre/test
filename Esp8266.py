3cfa7d72559a31d3c0038c1afd0d5346e6689908
BaertTorre <torre.baert@student.howest.be>
2020-06-09 14:32:18 +0200

    cleanup

--- Esp8266.py
+++ Esp8266.py
@@ -1,6 +1,6 @@
 import serial
 
-ser = serial.Serial('/dev/serial0')     # open serial port (serial0 is altijd de seriele poort en geen bluetooth ongeacht de config)
+ser = serial.Serial('/dev/ttyS0')     # open serial port (serial0 is altijd de seriele poort en geen bluetooth ongeacht de config)
 
 class Esp8266:
     @staticmethod
@@ -13,3 +13,5 @@ class Esp8266:
         ser.write(b'LDR')                     # write a string de 'b' zorgt ervoor dat de tekst gecodeert wordt in bytes
         data = Esp8266.read_serial()
         return data
+
+print(Esp8266.read_LDR())
--- servo2 beter.py
+++ servo2 beter.py
@@ -5,7 +5,7 @@ servo_pin = 16
 try:
     pi = pigpio.pi()
     while True:
-        puls_width = int(input('puls with >  '))        # bij elke servo anders, min 530, max 2400
+        puls_width = int(input('puls with >  '))        # bij elke servo anders, bij mij min 530, max 2400
         pi.set_servo_pulsewidth(servo_pin, puls_width)
 except KeyboardInterrupt as e:
     print(e)
